67fdee63a74c190e088d5866f92f27f86f6e499e
guest01 <guest01@FLE207D28.alunos.sc.senac.br>
2022-06-24 21:20:50 -0300

    agora

--- poo-entra21/src/listaexercicios1/Exercicio1.java
+++ poo-entra21/src/listaexercicios1/Exercicio1.java
@@ -4,7 +4,26 @@ public class Exercicio1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		int[]numeros = new int [10];
+		
+		
+		//2 - preencher o array
+		
+		numeros [0] =  2;	
+		numeros [1] = 11;
+		numeros [3] = 0 ;
+		numeros [4] = 0 ;
+		numeros [5] = 0 ;
+		numeros [6] = 0 ;
+		numeros [7] = 0 ;
+		numeros [8] = 0 ;
+		numeros [9] = 0 ;
+		
+		//3 - mostrar na rela (console) os valores pares
+		// percorrer todo o array
+		// para cada item
+		// se ele for par -> mostrar na tela
 	}
 
 }
